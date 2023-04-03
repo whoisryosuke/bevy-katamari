bae30d057e580a4d4590c864fac1e004560c9c19
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-04-03 14:37:04 -0700

    :sparkles: physics movement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
+// The Player object
+#[derive(Component)]
+struct Player;
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(
@@ -13,6 +17,7 @@ fn main() {
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup_graphics)
         .add_startup_system(setup_physics)
+        .add_system(move_player)
         .run();
 }
 
@@ -40,7 +45,7 @@ pub fn setup_physics(
         Collider::cuboid(ground_size, ground_height, ground_size),
         PbrBundle {
             mesh: meshes.add(shape::Plane::from_size(ground_size).into()),
-            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            material: materials.add(Color::hex("#DDDDDD").unwrap().into()),
             transform: Transform::from_xyz(0.0, -ground_height, 0.0),
             ..default()
         },
@@ -48,17 +53,33 @@ pub fn setup_physics(
 
     commands.spawn((
         RigidBody::Dynamic,
+        Player,
         Collider::ball(2.0),
         ColliderDebugColor(Color::hsl(220.0, 1.0, 0.3)),
+        Velocity::default(),
         PbrBundle {
             mesh: meshes.add(Mesh::from(shape::UVSphere {
                 radius: 1.0,
                 sectors: 16,
                 stacks: 8,
             })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            material: materials.add(Color::rgb(0.0, 0.15, 0.8).into()),
             transform: Transform::from_xyz(0.0, 10.0, 0.0),
             ..default()
         },
     ));
+}
+
+fn move_player(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Velocity, With<Player>>) {
+    let mut player_velocity = query.single_mut();
+
+    println!("Player velocity: {}", player_velocity.linvel);
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        player_velocity.linvel -= Vec3::new(1.0, 0.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        player_velocity.linvel += Vec3::new(1.0, 0.0, 0.0);
+    }
 }
